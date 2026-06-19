3588deeb17a9b30da6a436cdc5a6bba7728a67d0
Lyheang <srunlyheangtheapple@gmail.com>
2026-06-20 01:15:25 +0700

    welcomeemail

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,14 +1,8 @@
 import jwt from "jsonwebtoken"
-<<<<<<< HEAD
 import { ENV } from "./env.js";
 
 export const generateToken = (userID, res) => {
     const {JWT_SECRET} = ENV;
-=======
-
-export const generateToken = (userID, res) => {
-    const {JWT_SECRET} = process.env;
->>>>>>> 457c702afcc6c769ce6866198198b0fd87e2692b
     if (!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
     }
@@ -19,11 +13,7 @@ export const generateToken = (userID, res) => {
         maxAge: 7 * 24 * 60 * 60 *1000, //miliseconds
         httpOnly: true, //prevent XSS attacks: cross-site scripting
         sameSite: "strict", //CSRF attacts
-<<<<<<< HEAD
         secure: ENV.NODE_ENV === "development" ? false: true,
-=======
-        secure: process.env.NODE_ENV === "development" ? false: true,
->>>>>>> 457c702afcc6c769ce6866198198b0fd87e2692b
     });
     return token;
 };
