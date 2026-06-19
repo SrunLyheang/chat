57c240f69d5edc95bca9832c710d15f3a0987b06
Lyheang <srunlyheangtheapple@gmail.com>
2026-06-19 12:41:59 +0700

    sign up controller added

---
+++ backend/lib/utils.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userID, res) => {
+    const token = jwt.sign({userID}, process.env.JWT_SECRET,{
+        expiresIn: "7d",
+    } );
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 *1000, //miliseconds
+        httpOnly: true, //prevent XSS attacks: cross-site scripting
+        sameSite: "strict", //CSRF attacts
+        secure: process.env.NODE_ENV === "development" ? false: true,
+    });
+    return token;
+};
+
