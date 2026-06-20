d03e534f3d5f73e1e2437c377d7ae348d835541e
Lyheang <srunlyheangtheapple@gmail.com>
2026-06-20 21:53:02 +0700

    rafactor

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,10 +4,11 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 
-router.post("/login", arcjetProtection, login);
+router.post("/login", login);
 
 router.post("/logout", logout);
 
