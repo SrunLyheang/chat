18424369fa732baa63106dc74e7f9a4feb7c8a91
Lyheang <srunlyheangtheapple@gmail.com>
2026-06-21 15:15:47 +0700

    message route

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-    try{
-        const {MONGO_URI} = ENV;
+    try {
+        const { MONGO_URI } = ENV;
         if (!MONGO_URI) throw new Error("MONGO_URI is not set")
         const conn = await mongoose.connect(ENV.MONGO_URI)
         console.log("MONGODB CONNECTED", conn.connection.host)
 
-    } catch (error){
-    console.error("ERROR CONNECTING TO MONGODB" , error)
-    process.exit(1); // 1 status code means fail.0 means success
+    } catch (error) {
+        console.error("ERROR CONNECTING TO MONGODB", error)
+        process.exit(1); // 1 status code means fail.0 means success
 
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,19 @@
 
 import express from "express";
-
+import arcjetProtection from "@arcjet/node";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.get("/send", (req,res) => {
-    res.send("Send message endpoint")
-});
-   
+// the middleware executes in order -so request get rate limited first, then authenticated
+// this is more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+
+
 
 export default router;
