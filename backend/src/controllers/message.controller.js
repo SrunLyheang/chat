57c5ee95de0d80badfd89156b4c1ee00695e7299
Lyheang <srunlyheangtheapple@gmail.com>
2026-06-26 01:00:55 +0700

    Added Chatheader and added loading skeletons for mesages load

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -27,7 +27,7 @@ export const getMessagesByUserId = async (req, res) => {
         { senderId: userToChatId, receiverId: myId },
       ],
     });
-    res.status(200).jsonn(messages)
+    res.status(200).json(messages)
 
   } catch (error) {
     console.log("Error in getMessages controller", error.message);
